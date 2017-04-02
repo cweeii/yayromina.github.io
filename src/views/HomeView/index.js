6c47932e9651faae184e499451a43f329f105ff1
RominaMJD <romina@mjdinteractive.com>
2017-04-01 20:29:43 -0700

    test

--- src/views/HomeView/index.js
+++ src/views/HomeView/index.js
@@ -5,7 +5,7 @@ export default class HomeView extends React.Component {
   render() {
     return (
       <div style={styles.base}>
-        <h1>Romina Barrett</h1>
+        <h1>It's about to get weird</h1>
       </div>
     );
   }
