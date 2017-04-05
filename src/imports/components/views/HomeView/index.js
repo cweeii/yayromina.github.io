50002ce78b1aad1499adc8e75e5dd7e0f360257e
RominaMJD <romina@mjdinteractive.com>
2017-04-04 21:44:01 -0700

    basic bouncing

--- src/imports/components/views/HomeView/index.js
+++ src/imports/components/views/HomeView/index.js
@@ -6,23 +6,37 @@ import styles from './styles';
 export default class HomeView extends React.Component {
 
   componentDidMount() {
-    // this.animate();
+    this.animate();
   }
 
   animate = () => {
     anime({
-      translateY: 250,
-      scale: 2,
-      easing: 'easeInOutQuart',
-      duration: 2000,
-      elasticity: 1,
-      targets: this.headline,
+      translateY: '300px',
+      duration: 300,
+      direction: 'alternate',
+      easing: 'easeInCubic',
+      scaleX: {
+        value: 1.44,
+        duration: 150,
+        delay: 268,
+      },
+      scaleY: {
+        value: 0.95,
+        duration: 125,
+        delay: 268,
+      },
+      loop: 4,
+      targets: this.logo,
     });
   }
   render() {
     return (
       <div style={styles.base}>
-        <Logo />
+        <div style={styles.logoWrapper}>
+          <div style={styles.logo} ref={(el) => { this.logo = el; }}>
+            <Logo />
+          </div>
+        </div>
         <div style={styles.headlines}>
           <h1 ref={(el) => { this.headline = el; }}>Romina Barrett</h1>
           <h2>Senior Web Engineer &amp; Animator</h2>
--- src/imports/components/views/HomeView/styles.js
+++ src/imports/components/views/HomeView/styles.js
@@ -3,12 +3,28 @@ export default {
     height: '100vh',
     width: '100%',
     display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
     alignItems: 'center',
     justifyContent: 'center',
   },
   headlines: {
+    display: 'none',
     lineHeight: '2.6rem',
     textAlign: 'center',
     width: '100%',
+  },
+  logoWrapper: {
+    paddingTop: '100px',
+    width: '100%',
+  },
+  logo: {
+    height: 100,
+    width: 100,
+    margin: 'auto',
+    display: 'block',
+    opacity: 1,
+    position: 'relative',
+    top: '-250px',
   },
 };
